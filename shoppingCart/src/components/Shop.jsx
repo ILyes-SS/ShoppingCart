a12edd623e7ca8a1c59190f7aafecd5110b67b2a
ilyes <155851163+Senpai-cod@users.noreply.github.com>
2024-11-24 21:46:23 +0100

    Add the ShopCard components that shows a product details
    added products to Shop page, products quantity
    can be incremented or decremented or enterd manually

--- shoppingCart/src/components/Shop.jsx
+++ shoppingCart/src/components/Shop.jsx
@@ -1,8 +1,11 @@
 import { LoaderCircle } from "lucide-react";
 import useProducts from "./useProducts";
+import { useOutletContext } from "react-router-dom";
+import ShopCard from "./ShopCard";
 
 export default function Shop() {
   const { products, error, loading } = useProducts("5");
+  const [cart, setCart] = useOutletContext();
 
   if (error) return <h1>Oops!</h1>;
   if (loading)
@@ -16,16 +19,7 @@ export default function Shop() {
       <>
         {products.map((prod) => {
           return (
-            <div className="card" key={prod.id} style={{ width: "200px" }}>
-              <img
-                src={prod.image}
-                alt={prod.title}
-                style={{ width: "200px" }}
-              />
-              <h2>{prod.title}</h2>
-              <p>{prod.price}</p>
-              <button>Add to Cart</button>
-            </div>
+            <ShopCard {...prod} key={prod.id} cart={cart} setCart={setCart} />
           );
         })}
       </>
--- shoppingCart/src/components/ShopCard.jsx
+++ shoppingCart/src/components/ShopCard.jsx
@@ -1,3 +1,74 @@
-export default ShopCard(){
-    return()
+import { useState } from "react";
+export default function ShopCard({ title, image, price, cart, setCart }) {
+  const [value, setValue] = useState(0);
+
+  let isAdded = false;
+  if (cart.some((obj) => obj.title == title)) {
+    isAdded = true;
+  }
+  function changeHandler(e) {
+    setValue(e.target.value);
+  }
+  function submitHandler() {
+    let index = cart.findIndex((obj) => obj.title == title);
+    const newQuantity = Number(value);
+    setValue(newQuantity);
+    setCart((prev) =>
+      prev.toSpliced(
+        index,
+        1,
+        Object.assign({}, cart[index], { quantity: newQuantity })
+      )
+    );
+  }
+  // console.table(cart);
+  function addToCart() {
+    setValue(1);
+    setCart((prev) => [...prev, { title, image, price, quantity: 1 }]);
+  }
+  function decrementQuantity() {
+    let index = cart.findIndex((obj) => obj.title == title);
+    const newQuantity = cart[index].quantity - 1;
+    setValue(newQuantity);
+    setCart((prev) =>
+      cart[index].quantity - 1 == 0
+        ? prev.toSpliced(index, 1)
+        : prev.toSpliced(
+            index,
+            1,
+            Object.assign({}, cart[index], { quantity: newQuantity })
+          )
+    );
+  }
+  function incrementQuantity() {
+    let index = cart.findIndex((obj) => obj.title == title);
+    const newQuantity = cart[index].quantity + 1;
+    setValue(newQuantity);
+    setCart((prev) =>
+      prev.toSpliced(
+        index,
+        1,
+        Object.assign({}, cart[index], { quantity: newQuantity })
+      )
+    );
+  }
+  return (
+    <div className="card" style={{ width: "200px" }}>
+      <img src={image} alt={title} style={{ width: "200px" }} />
+      <h2>{title}</h2>
+      <p>{price}</p>
+      {isAdded ? (
+        <div>
+          <div>
+            <button onClick={() => decrementQuantity()}>-</button>
+            <input type="text" onChange={changeHandler} value={value} />
+            <button onClick={() => incrementQuantity()}>+</button>
+          </div>
+          <button onClick={() => submitHandler()}>Submit quantity</button>
+        </div>
+      ) : (
+        <button onClick={() => addToCart()}>Add to Cart</button>
+      )}
+    </div>
+  );
 }
