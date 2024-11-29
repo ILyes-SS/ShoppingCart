2a1a150371c42016fed16b4ca7b6b0d8bddf02b4
ilyes benmalti <benmaltiilyes@gmail.com>
2024-11-29 08:00:44 +0100

    I accidently pushed additional folders so i deleted them

--- routerFollowAlong/src/Button.jsx
+++
@@ -1,4 +0,0 @@
-import styles from "./Button.module.css";
-export default function Button({ type }) {
-  return <button className={styles[type]}>yes!</button>; //composes aint working so just use multiple classes
-}
